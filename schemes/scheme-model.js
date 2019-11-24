d5837590c38532bf92bbeec50ef6b7a9a62d5dcf
Jashele T <jashele@Jasheles-Air.domain>
2019-11-23 19:54:43 -0700

    find steps code added and /api/schemes/3/steps works in postman

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,7 +9,20 @@ function findById(id) {
 }
 
 function findSteps(id) {
-
+return db("steps") // steps.js
+.join(
+   "schemes", // steps.js + schemes.js
+   "schemes.id", "steps.scheme_id" // attaching what scheme_id in steps is referencing from schemes.id
+)
+.select(
+   "steps.id", // will display the id of the step
+   "schemes.scheme_name", // will display scheme_name from schemes.js
+   "steps.step_number", // will display step_number from steps.js
+   "steps.instructions" // will display instructions from steps.js
+)
+.where({
+   scheme_id: id 
+})
 }
 
 function add(scheme) {
