fa705901968eff8b88c58d12472457e07918710b
Jashele T <jashele@Jasheles-Air.domain>
2019-11-23 18:34:29 -0700

    place in empty functions in model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,16 @@
+const db = require('../db-config.js')
+
+function find() {
+}
+
+function findById(id) {
+}
+
+function add(user) {
+}
+
+function update(changes, id) {
+}
+
+function remove(id) {
+}
