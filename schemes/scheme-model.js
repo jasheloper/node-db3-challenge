2925e62df51b7354bad624558abe22d8ad1f0e72
Jashele T <jashele@Jasheles-Air.domain>
2019-11-23 18:47:07 -0700

    helper functions are in and exported

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,16 +1,33 @@
-const db = require('../db-config.js')
+const db = require("../db-config.js");
 
 function find() {
+  return db("schemes");
 }
 
 function findById(id) {
+  return db("schemes").where({ id });
 }
 
-function add(user) {
+function add(schemeData) {
+  return db("schemes").insert(schemeData);
 }
 
 function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
 }
 
 function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
 }
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  remove
+};
